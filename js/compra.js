5f2e864aa3b7b27f38a9511f4df7e30d9a5769ff
mauri <utn.mauri@gmail.com>
2021-09-09 14:43:12 -0300

    rediseño del nabvar

--- js/compra.js
+++ js/compra.js
@@ -1,23 +1,51 @@
-
+const carrito = [];
+let contador = 0;
 
 function comprar(CodPd) {
   let cantidad = parseInt(prompt("Ingrese Cantidad a comprar:"));
-  let rs = 0;
-  const pd = impulsivos.filter((b) => {
-    let p = 0,
-      st = 0,
-      stockD = 0;
-    if (b.id === CodPd) {
-      if (cantidad > b.stock) {
-        alert(`cantidad insuficiente, el stock disponible es de:${b.stock}`);
-      } else {
-        p = b.Precio;
-        st = b.stock;
-        rs = cantidad * p;
-        stockD = st - cantidad;
-        alert("Total a pagar:$ " + rs);
-        alert("Gracias por su compra!");
+  if (isNaN(cantidad)) {
+    alert("Debe ingresar un número");
+    //comprar();
+  } else {
+    let rs = 0;
+    const add = [];
+    const pd = impulsivos.filter((b) => {
+      let p = 0,
+        st = 0,
+        stockD = 0;
+      if (b.id === CodPd) {
+        if (cantidad > b.stock) {
+          alert(`cantidad insuficiente, el stock disponible es de:${b.stock}`);
+        } else {
+          p = b.Precio;
+          st = b.stock;
+          rs = cantidad * p;
+          stockD = st - cantidad;
+          add.push(b.descricpion, cantidad, b.Precio, rs);
+          carrito.push(add);
+          contador += cantidad;
+          let cart = document.getElementById("carrito_cantidad");          
+          cart.innerHTML = `${contador}`;
+          alert("Total a pagar:$ " + rs);
+          //alert("Gracias por su compra!");
+          return carrito;
+        }
       }
-    }
-  });
+    });
+  }
+}
+/* 
+function addCarrito() {
+  const cars = [];
+  //const filtrado = agregado.filter((b) => b.id !== b.id);
+  cars.push(carrito); 
+  //for (const x of cars) {
+ 
+  //}
+}
+ */
+function finCompra() {
+  alert(carrito);
+  let total = carrito.rs;
+  console.log(carrito);
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 const impulsivos = [
   {
     id: 1,
@@ -19,7 +17,7 @@ const impulsivos = [
     id: 3,
     descricpion: "Bombon Suizo",
     Precio: 340,
-    img: "./img/bombonvainilla.png",
+    img: "./img/bombom_suizo_caja.jpeg",
     stock: 20,
   },
   {
@@ -47,7 +45,7 @@ const impulsivos = [
     id: 7,
     descricpion: "Delicia",
     Precio: 270,
-    img: "./img/delicia.jpeg",
+    img: "./img/delicia.png",
     stock: 20,
   },
   {
@@ -105,24 +103,61 @@ const impulsivos = [
     Precio: 580,
     img: "./img/torta_milka.jpg",
     stock: 20,
-  }
+  },
+
+  {
+    id: 16,
+    descricpion: "Torta Oreo",
+    Precio: 580,
+    img: "./img/torta_oreo.png",
+    stock: 20,
+  },
+  {
+    id: 17,
+    descricpion: "Familiar Nº2",
+    Precio: 580,
+    img: "./img/familiar_n2.png",
+    stock: 20,
+  },
 ];
 
+let count = 0;
+/* function sumar() {
+  if (count < stock) {
+    count = count + 1;
+  }
+}
+function restar() {
+  if (count > 1) {
+    count--;
+  }
+} */ /* 
+  const addCart = () => {
+      onAdd(count)
+  } */
 
+  
 
 let c = document.getElementById("catalogo");
-
 const pdc = impulsivos;
-for (c of pdc) {
-  catalogo.innerHTML += `<div class="card justify-content-evenly" style="width: 18rem;">
-    <img src="./${c.img}" class="card-img-top" alt="Bombon Escoces caja">
+for (const p of pdc) {
+  c.innerHTML += `<div class="card" style="width: 18rem;">
+    <img src="./${p.img}" class="card-img-top" alt="Bombon Escoces caja">
     <div class="card-body">
-      <h5 class="card-title"><strong> ${c.descricpion}</strong></h5>
-      <p class="card-text" id="CodPd">Codigo Producto: ${c.id}</p>
-      <p class="card-text">Descripcion ${c.descricpion}</p>
-      <p class="card-text" id="precio">Precio $${c.Precio}</p>
-      <p class="card-text">Stock Disponible: <span id="stock">${c.stock}</span></p>
-      <a href="#" class="btn btn-primary"id="comprar" onclick="comprar(${c.id})">Comprar</a>
+      <h5 class="card-title"><strong> ${p.descricpion}</strong></h5>
+      <p class="card-text" id="CodPd">Codigo Producto: ${p.id}</p>
+      <p class="card-text">Descripcion ${p.descricpion}</p>
+      <p class="card-text" id="precio">Precio $${p.Precio}</p>
+      <p class="card-text">Stock Disponible: <span id="stock">${p.stock}</span></p>
+      <a href="#" class="btn btn-primary"id="comprar" onclick="comprar(${p.id})">Comprar</a>
+        <div class='addCarrito'>
+                  <div>
+                      <button id='btnResta' class='btn-primary sumres res' onClick=restar()>-</button>
+                      <span>${count}</span>
+                      <button id='btnSuma' class='btn-primary sumres sum' onClick=sumar()>+</button>
+                  </div>
+                  <button id='btnAdd' onClick=addCart() class='btn-primary addcart'>Add to Cart</button>
+        </div>
     </div>
   </div>`;
 }
