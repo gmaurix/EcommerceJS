d7c1be3cffe5bad69f5ebb86688e05ec647f2c61
mauri <utn.mauri@gmail.com>
2021-09-20 17:47:22 -0300

    Se incorpora JQuery

--- js/compra.js
+++ js/compra.js
@@ -3,9 +3,15 @@ let contador = 0;
 let total = 0;
 
 function comprar(CodPd) {
+  let msj = $("#mensaje"); //JQuery
   cantidad = count;
   if (isNaN(cantidad) || cantidad <= 0) {
-    alert("Debe ingresar un número mayor que cero");
+    /* ------------------ Jquery para mostrar en el DOM mensajes ----------------- */
+    msj
+      .css({ color: "#9e0909" })
+      .fadeIn("slow")
+      .text("Debe ingresar un número mayor que cero")
+      .fadeOut(5500);
   } else {
     let rs = 0;
     const add = [];
@@ -15,21 +21,33 @@ function comprar(CodPd) {
         stockD = 0;
       if (b.id === CodPd) {
         if (cantidad > b.stock) {
-          alert(`Cantidad insuficiente, el stock disponible es de: ${b.stock}`);
+          /* ------------------ Jquery para mostrar en el DOM mensajes ----------------- */
+          msj
+            .css({ color: "#9e0909" })
+            .text(
+              `Cantidad insuficiente, el stock disponible es de: ${b.stock}`
+            )
+            .fadeIn("slow");
         } else {
           p = b.Precio;
           st = b.stock;
+          d = b.descripcion;
           rs = cantidad * p;
           stockD = st - cantidad;
-          add.push(b.descricpion, cantidad, b.Precio, rs);
+          add.push(d, cantidad, b.Precio, rs);
           carrito.push(add);
-          contador += cantidad;
+          contador = cantidad;
+          cantidad = 0;
           total += rs;
-          let cart = document.getElementById("carrito_cantidad");
-          // let monto = document.getElementById("montoTotal");
-          cart.innerHTML = `${contador}`;
-          // monto.innerHTML = `${total}`;
-          //alert("Total a pagar:$ " + rs);
+          /* ------------------ Jquery para manipular el DOM ----------------- */
+          let cart = $("#carrito_cantidad");
+          let monto = $(".importe");
+
+          monto.css({ color: "#139713", "font-weight": "bold" });
+          monto.fadeIn(900).text(`Importe Total: $${total}`).fadeOut(8500);
+
+          cart.text(`${contador}`);
+
           return carrito;
         }
       }
@@ -38,7 +56,6 @@ function comprar(CodPd) {
 }
 
 function finCompra() {
-  alert(carrito);
   const jsonCarro = JSON.stringify(carrito);
   localStorage.setItem("jsCarrito", jsonCarro);
   const pdLS = localStorage.getItem("jsCarrito");
--- js/main.js
+++ js/main.js
@@ -1,181 +1,239 @@
 const impulsivos = [
   {
     id: 1,
-    descricpion: "Bombon Escoces",
+    categoria: "Bombones",
+    descripcion: "Bombon Escoces",
     Precio: 390,
     img: "./img/bombon_escoces_caja.jpeg",
     stock: 15,
   },
   {
     id: 2,
-    descricpion: "Bombon Crocante",
+    categoria: "Bombones",
+    descripcion: "Bombon Crocante",
     Precio: 300,
     img: "./img/bomboncrocantesolo.png",
     stock: 10,
   },
   {
     id: 3,
-    descricpion: "Bombon Suizo",
+    categoria: "Bombones",
+    descripcion: "Bombon Suizo",
     Precio: 340,
     img: "./img/bombom_suizo_caja.jpeg",
     stock: 20,
   },
   {
     id: 4,
-    descricpion: "Bombon Vainilla Split",
+    categoria: "Bombones",
+    descripcion: "Bombon Vainilla Split",
     Precio: 300,
     img: "./img/bombonvainilla.png",
     stock: 10,
   },
   {
     id: 5,
-    descricpion: "Cassata",
+    categoria: "Postres",
+    descripcion: "Cassata",
     Precio: 270,
     img: "./img/casatta.png",
     stock: 20,
   },
   {
     id: 6,
-    descricpion: "Almendrado",
+    categoria: "Postres",
+    descripcion: "Almendrado",
     Precio: 270,
     img: "./img/almendrado.png",
     stock: 20,
   },
   {
     id: 7,
-    descricpion: "Delicia",
+    categoria: "Postres",
+    descripcion: "Delicia",
     Precio: 270,
     img: "./img/delicia.png",
     stock: 20,
   },
   {
     id: 8,
-    descricpion: "Crocantino",
+    categoria: "Postres",
+    descripcion: "Crocantino",
     Precio: 270,
     img: "./img/crocantino.png",
     stock: 20,
   },
   {
     id: 9,
-    descricpion: "Palito Bombon",
+    categoria: "Palitos",
+    descripcion: "Palito Bombon",
     Precio: 500,
     img: "./img/palito_bombon.jpeg",
     stock: 20,
   },
   {
     id: 10,
-    descricpion: "Palito Frutal Frutilla",
+    categoria: "Palitos",
+    descripcion: "Palito Frutal Frutilla",
     Precio: 300,
     img: "./img/palito_frutal_frutilla.png",
     stock: 20,
   },
   {
     id: 11,
-    descricpion: "Palito Frutal Limon",
+    categoria: "Palitos",
+    descripcion: "Palito Frutal Limon",
     Precio: 300,
     img: "./img/palitolimon.png",
     stock: 20,
   },
   {
     id: 12,
-    descricpion: "Palito Frutal Naranja",
+    categoria: "Palitos",
+    descripcion: "Palito Frutal Naranja",
     Precio: 300,
     img: "./img/palitonaranja.png",
     stock: 20,
   },
   {
     id: 13,
-    descricpion: "Palito Pico Dulce",
+    categoria: "Palitos",
+    descripcion: "Palito Pico Dulce",
     Precio: 300,
     img: "./img/picodulce__1.png",
     stock: 20,
   },
   {
     id: 14,
-    descricpion: "Torta Grido",
+    categoria: "Tortas",
+    descripcion: "Torta Grido",
     Precio: 580,
     img: "./img/torta_grido.png",
     stock: 6,
   },
   {
     id: 15,
-    descricpion: "Torta Milka",
+    categoria: "Tortas",
+    descripcion: "Torta Milka",
     Precio: 580,
     img: "./img/torta_milka.jpg",
     stock: 20,
   },
   {
     id: 16,
-    descricpion: "Torta Oreo",
+    categoria: "Tortas",
+    descripcion: "Torta Oreo",
     Precio: 580,
     img: "./img/torta_oreo.png",
     stock: 20,
   },
   {
     id: 17,
-    descricpion: "Familiar Nº1",
+    categoria: "Familiares",
+    descripcion: "Familiar Nº1",
     Precio: 580,
     img: "./img/familiar01.png",
     stock: 6,
   },
   {
     id: 18,
-    descricpion: "Familiar Nº2",
+    categoria: "Familiares",
+    descripcion: "Familiar Nº2",
     Precio: 580,
     img: "./img/familiar02.png",
     stock: 6,
   },
   {
     id: 19,
-    descricpion: "Familiar Nº3",
+    categoria: "Familiares",
+    descripcion: "Familiar Nº3",
     Precio: 580,
     img: "./img/familiar03.png",
     stock: 6,
   },
   {
     id: 20,
-    descricpion: "Familiar Nº4",
+    categoria: "Familiares",
+    descripcion: "Familiar Nº4",
     Precio: 580,
     img: "./img/familiar04.png",
     stock: 5,
   },
 ];
-let c = document.getElementById("catalogo");
-const pdc = impulsivos;
+
+let c = $("#catalogo");
 let count = 0;
+const pdc = impulsivos;
 
-let lb = document.getElementById("lbl");
+/* ------------------- funciones de botones de add carrito ------------------ */
 
-function sumar() {
-  if (count < 10) {
-    count++;
-    lbl.innerHTML = count;
-    console.log(count);
-  }
+/* ----------------- metodo para la opcion buscar del navar ----------------- */
+/* let dato = document.getElementById("buscar").value;
+
+function busqueda() {
+  const resultado = pdc.filter((p) => p.categoria.includes(dato));
+  return resultado;
 }
-function restar() {
-  if (count > 1) {
-    count--;
-    lb.innerHTML = count;
-  }
-}
+ */
+/* ---------------------------------- ---- ---------------------------------- */
+
 for (const p of pdc) {
-  c.innerHTML += `<div class="card" style="width: 14rem;">
-    <img src="./${p.img}" class="card-img-top" alt="Bombon Escoces caja">
+  c.append(`<div class="card" style="width: 14rem;">
+    <img src="./${p.img}" class="card-img-top" alt="helado" loading="lazy">
     <div class="card-body">
-      <h5 class="card-title"><strong> ${p.descricpion}</strong></h5>
+      <h5 class="card-title"><strong> ${p.descripcion}</strong></h5>
       <p class="card-text" id="CodPd" hidden=true>Codigo Producto: ${p.id}</p>
-      <p class="card-text" hidden=true >Descripcion ${p.descricpion}</p>
+      <p class="card-text" hidden=true >Descripcion ${p.descripcion}</p>
       <p class="card-text" id="precio">Precio $${p.Precio}</p>
       <p class="card-text">Stock Disponible: <span id="stock">${p.stock}</span></p>
-      <div>
-        <div>
-            <button type="button" class="btn btn-primary" onclick="restar()" >-</button>
-            <label id="lbl">${count}</label>
-            <button type="button" class="btn btn-primary" onclick="sumar()">+</button>
-            <a href="#" class="btn btn-primary"id="comprar" onclick="comprar(${p.id}),${count}">Agregar</a>
-      </div>        
+    <div>
+       <div>
+           <button type="button" class="b-resta btn btn-primary"
+             id="btn-restar${p.id}">-</button>
+            <label class="lbl" id="lbl${p.id}"></label>
+            <button type="button" class="b-suma btn btn-primary"
+            id="btn-sumar${p.id}">+</button>
+            <a href="#" class="btn btn-primary " id="comprar" onclick="comprar(${p.id}),${count}">Agregar</a>
+        </div>        
       </div>  
     </div>
-  </div>`;
+  </div>`);
+  $(`#btn-sumar${p.id}`).on("click", () => {
+    let ids = $(`#lbl${p.id}`);
+    for (let i of ids) {
+      count++;
+      ids.text(count);
+    }
+  });
 }
+
+let endBay = $("#finCompra");
+
+endBay.on("click", () => {
+  const pdLS = localStorage.getItem("jsCarrito");
+  const pds = JSON.parse(pdLS);
+  /* --------------
+  $("#importe").append(`
+    <div id="terminarCompra">
+                <table border ="1">
+                    <td>
+                          <tr> Producto</tr>
+                          <tr> ${pds.descripcion}</tr>
+                    </td>                  
+                    <td>
+                          <tr> Precio Unitario</tr>
+                          <tr> ${pds.Precio}</tr>
+                    </td>                 
+                    <td>
+                          <tr> Cantidad</tr>
+                          <tr> ${pds.can}</tr>
+                    </td>
+                    <td>
+                          <tr>Subtotal</tr>
+                          <tr> ${pds.descripcion}</tr>
+                    </td>                                
+                </table>
+            </div>
+    `),
+    $(".compra").fadeIn(5000); */
+});
